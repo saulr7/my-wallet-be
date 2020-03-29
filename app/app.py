ea1943b617b84aa529c0fb8dc2c82666bbe8f7e3
Fernando Ramirez <fernandoramirez@Fernandos-MacBook-Pro.local>
2020-03-29 02:32:14 -0600

    heroku

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from routes import routes , user_routes, transaction_routes
+# from routes import routes , user_routes, transaction_routes
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(routes.category_routes)
-app.register_blueprint(user_routes.user_routes)
-app.register_blueprint(transaction_routes.transaction_routes)
+# app.register_blueprint(routes.category_routes)
+# app.register_blueprint(user_routes.user_routes)
+# app.register_blueprint(transaction_routes.transaction_routes)
 
 @app.route('/')
 def index():
