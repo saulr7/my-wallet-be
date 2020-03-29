c3120f4c23f0182cf14f0db561f47202e7dbab31
Fernando Ramirez <fernandoramirez@Fernandos-MacBook-Pro.local>
2020-03-29 01:40:45 -0600

    moved folders

---
+++ app/config/BD.py
@@ -0,0 +1,11 @@
+
+_DRIVER      =   "{PostgreSQL Unicode}"
+_DATABASE    =   "dd836m8fdl2fmf"
+_UID         =   "nkfwellcwwybmi"
+_PWD         =   "cd1610b2ebbb6beb12f0e181900e732e552d2b04322e4b4b37f88f3ac206db83"
+_SERVER      =   "ec2-3-223-21-106.compute-1.amazonaws.com"
+_PORT        =   "5432 "
+
+
+CONNECT_STR = "dbname=" +_DATABASE+" user="+_UID+" host= "+ _SERVER + " password= "+_PWD+""
+    
