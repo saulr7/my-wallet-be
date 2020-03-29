d48d8a38e4b36fed01ad0033a4669d3027d5aa87
Fernando Ramirez <fernandoramirez@Fernandos-MacBook-Pro.local>
2020-03-29 15:17:55 -0600

    heroku

--- app/app.py
+++ app/app.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from app.routes import routes, transaction_routes, user_routes
 # from routes import routes , user_routes, transaction_routes
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     app.register_blueprint(routes.category_routes)
     app.register_blueprint(user_routes.user_routes)
     app.register_blueprint(transaction_routes.transaction_routes)
 
     @app.route('/', methods=['GET'])
+    @cross_origin()
     def index():
         return "Test", 200
 
